f344801f7060eb448f1708b47170ba3ead68a41a
fluviolobo <fluvio.lobo@gmail.com>
2016-06-23 15:30:58 +0000

    Created a directory for all software dvelopment (following the structure of other repos). Began populating the software directory with a python script that controls the raspberry pi camera module.

---
+++ Software/Python/pyCam.py
@@ -0,0 +1,25 @@
+"""
+pyCam.py
+
+The following script has been desveloped to control de Raspberry Pi camera module using Python
+
+Fluvio L. Lobo Fenoglietto 06/23/2016
+"""
+
+# =======================
+# Import Libraries and/or Modules
+# =======================
+
+import picamera
+
+# =======================
+# Variables
+# =======================
+
+picam = picamera.PiCamera()
+
+
+# =======================
+# Operation
+# =======================
+camera.start_preview()
