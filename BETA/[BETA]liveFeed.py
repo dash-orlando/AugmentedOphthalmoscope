ac04a7b148dc40ea135089fe19dfa3edf6dd19d8
moe93 <m.odeh93@gmail.com>
2016-08-15 15:23:41 -0400

    Improved reliability of program

--- BETA/[BETA]liveFeed.py
+++ BETA/[BETA]liveFeed.py
@@ -7,39 +7,46 @@
 '''
 print __doc__
 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import SimpleCV
+import time
 import cv2
 import numpy
-from io import BytesIO
-from time import sleep
-from picamera import PiCamera
 
+# Setup camera and display environment
 display = SimpleCV.Display(resolution = (640,480), title = "Live Feed ver0.1")
-cam = PiCamera()
-cam.resolution = (640, 480)
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(640,480))
 normaldisplay = True
+time.sleep(0.1)
 
 while display.isNotDone():
     if display.mouseRight:
         normaldisplay = not(normaldisplay)
         print "Display Mode:", "Normal" if normaldisplay else "Segmented" 
     
-    stream = BytesIO()
-    #give time to initialize
-    sleep(2)
-    cam.capture(stream, format='jpeg')
-    buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
-    cv2img = cv2.imdecode(buff,1)
-    cv2RGB = cv2.cvtColor(cv2img, cv2.COLOR_BGR2RGB)
-    img = SimpleCV.Image(cv2RGB)
-    dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
-    segmented = img.stretch(200,255)
-    blobs = segmented.findBlobs()
+    # Start capturing image stream
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image = frame.array
+        cv2RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img = SimpleCV.Image(cv2RGB)
+        dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
+        segmented = img.stretch(200,255)
+        blobs = segmented.findBlobs()
+        dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
+        segmented = img.stretch(200,255)
+        blobs = segmented.findBlobs()
     if blobs:
         circles = blobs.filter([b.isCircle(0.2) for b in blobs])
         if circles:
             img.drawCircle((circles[-1].x, circles[-1].y), circles[-1].radius(),SimpleCV.Color.BLUE,3)
-
+    cv2.imshow("test", image)
+    key = cv2.waitKey(1) & 0xFF
+    rawCapture.truncate(0)
+    
     if normaldisplay:
         img.show()
     else:
--- liveFeed.py
+++ liveFeed.py
@@ -5,31 +5,37 @@
 *       1- PiCamera is accessed as USB driver.
 *       2- UV4L package displays a banner on the live display
 *       3- Live display is blue-ish in color
+*       4- Low framerate and lag on output
 '''
 print __doc__
 
 import SimpleCV
 
-display = SimpleCV.Display()
+display = SimpleCV.Display(resolution = (640,480), title = "Live Feed ver0.1")
 cam = SimpleCV.Camera()
+cam.resolution = (640, 480)
 normaldisplay = True
 
+# Initiate Display
 while display.isNotDone():
+    if display.mouseRight:
+        normaldisplay = not(normaldisplay)
+        print "Display Mode:", "Normal" if normaldisplay else "Segmented" 
 
-	if display.mouseRight:
-		normaldisplay = not(normaldisplay)
-		print "Display Mode:", "Normal" if normaldisplay else "Segmented" 
-	
-	img = cam.getImage().flipHorizontal()
-	dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
-	segmented = dist.stretch(200,255)
-	blobs = segmented.findBlobs()
-	if blobs:
-		circles = blobs.filter([b.isCircle(0.5) for b in blobs])
-		if circles:
-			img.drawCircle((circles[-1].x, circles[-1].y), circles[-1].radius(),SimpleCV.Color.BLUE,3)
+    # Start feed
+    img = cam.getImage().flipHorizontal()
+    # Modify image for easier blob detection
+    dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
+    segmented = dist.stretch(200,255)
+    # If blobs are detected find circles
+    blobs = segmented.findBlobs()
+    if blobs:
+        # Filter circles
+        circles = blobs.filter([b.isCircle(0.2) for b in blobs])
+        if circles:
+            img.drawCircle((circles[-1].x, circles[-1].y), circles[-1].radius(),SimpleCV.Color.BLUE,3)
 
-	if normaldisplay:
-		img.show()
-	else:
-		segmented.show()
+    if normaldisplay:
+        img.show()
+    else:
+        segmented.show()
