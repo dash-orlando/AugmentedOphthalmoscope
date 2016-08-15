666e59ccd5f12aef38a733dbca2ebe7b3d428f30
moe93 <m.odeh93@gmail.com>
2016-08-15 15:35:19 -0400

    Version 0.3

--- liveFeed.py
+++ liveFeed.py
@@ -1,41 +1,71 @@
 '''
-* Track a white circle using SimpleCV
-* The parameters may need to be adjusted to match the RGB color the object.
-* Known issues:
-*       1- PiCamera is accessed as USB driver.
-*       2- UV4L package displays a banner on the live display
-*       3- Live display is blue-ish in color
-*       4- Low framerate and lag on output
+* VERSION: 0.3
+*   - Track any circular object instead of a specific color
+*   - Huge improvements to the framerate
+*   - Dropped the need for extra drivers and now runs picam module natively
+*
+* KNOWN ISSUES:
+*       1- Circle detection needs a lot of improvement and filtering
+*       2- Framerate still needs improvement
 '''
+
 print __doc__
 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import SimpleCV
+import time
+import cv2
+import numpy
 
-display = SimpleCV.Display(resolution = (640,480), title = "Live Feed ver0.1")
-cam = SimpleCV.Camera()
-cam.resolution = (640, 480)
+# Setup camera and display environment
+display = cv2.namedWindow("Live Feed ver0.2")
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 30
+rawCapture = PiRGBArray(camera, size=(640,480))
 normaldisplay = True
+time.sleep(0.1)
 
-# Initiate Display
-while display.isNotDone():
-    if display.mouseRight:
-        normaldisplay = not(normaldisplay)
-        print "Display Mode:", "Normal" if normaldisplay else "Segmented" 
+# Start capturing image stream
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+    #Reduce Noise
+    image = cv2.medianBlur(image,5)
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles(grayscale, cv2.cv.CV_HOUGH_GRADIENT,1.2,100,
+                               param1=55,param2=50,minRadius=1,maxRadius=50)
 
-    # Start feed
-    img = cam.getImage().flipHorizontal()
-    # Modify image for easier blob detection
+    if circles is not None:
+        circles = numpy.round(circles[0,:]).astype("int")
+        for (x, y, r) in circles:
+            # Draw circle
+            cv2.circle(image, (x,y),r,(0,255,0),4)
+
+    cv2.imshow("test", image)
+    key = cv2.waitKey(1) & 0xFF
+    rawCapture.truncate(0)
+'''
+    #ORIGINAL BEGIN
+    img = SimpleCV.Image(cv2RGB)
+    #dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
+    #segmented = img.stretch(200,255)
+    #blobs = segmented.findBlobs()
+    #ORIGINAL END
     dist = img.colorDistance(SimpleCV.Color.BLACK).dilate(2)
-    segmented = dist.stretch(200,255)
-    # If blobs are detected find circles
+    segmented = img.stretch(200,255)
     blobs = segmented.findBlobs()
     if blobs:
-        # Filter circles
         circles = blobs.filter([b.isCircle(0.2) for b in blobs])
         if circles:
-            img.drawCircle((circles[-1].x, circles[-1].y), circles[-1].radius(),SimpleCV.Color.BLUE,3)
-
+            for b in circles:
+                if int(b.radius()) > 1 and int(b.radius()) < 50:
+                    cv2.circle(image, (circles[-1].x, circles[-1].y), circles[-1].radius(),SimpleCV.Color.BLUE,3)
+    cv2.imshow("test", image)
+# BEGIN ''
     if normaldisplay:
-        img.show()
+        cv2.imshow("test", img)
     else:
         segmented.show()
+# END ''
+'''
