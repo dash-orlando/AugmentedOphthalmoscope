d808faa58e670ee2de3c4160afed32ef6fe82061
moe93 <m.odeh93@gmail.com>
2016-09-16 13:51:08 -0400

    Added argument flags and more functionality

---
+++ liveFeed.py
@@ -0,0 +1,166 @@
+'''
+* NOTE: Overaly must be specified using the -o or --overlay flag
+*
+* VERSION: 0.8.5
+*   - FIXED MAJOR CRASHING ISSUE!!!
+*   - Added various flags to fine tune code execution
+*       (i.e. python liveFeed.py -d 1 -a 0.25 -o /path/to/overlay.png)
+*
+* KNOWN ISSUES:
+*   - On occasion, circles are detected when there is non.
+*   - Overlay images used need some cleaning up.
+*
+* AUTHOR: Mohammad Odeh
+*
+* ----------------------------------------------------------
+*                           Instructions
+* ----------------------------------------------------------
+* 1) Place camera about 35mm away from target and make sure
+*    target is well illuminated.
+* 2) MAGIC!!
+*
+* RIGHT CLICK: Shutdown Program.
+* LEFT CLICK: Toggle view.
+'''
+
+ver = "Live Feed Ver0.8.5"
+print __doc__
+
+# Import necessary modules
+from imutils.video.pivideostream import PiVideoStream
+from time import sleep
+import argparse
+import cv2
+import numpy
+
+# Define placeholder function for trackbar
+def placeholder(x):
+    pass
+
+# Define right/left mouse click events
+def control(event, x, y, flags, param):
+    global normalDisplay
+    # Right button shuts down program
+    if event == cv2.EVENT_RBUTTONDOWN:
+        stream.stop()
+        cv2.destroyAllWindows()
+        quit()
+    # Left button toggles display
+    elif event == cv2.EVENT_LBUTTONDOWN:
+        normalDisplay=not(normalDisplay)
+
+# Construct argument parser
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--overlay", required=True,
+                help="path to overlay image")
+ap.add_argument("-a", "--alpha", type=float, default=0.85,
+                help="set alpha level (smaller = more transparent).\ndefault=0.85")
+ap.add_argument("-d", "--debug", type=int, default=0,
+                help="set flag equal to one (1) to enable debugging")
+args = vars(ap.parse_args())
+
+# Load overlay image with Alpha channel
+overlayImg = cv2.imread(args["overlay"], cv2.IMREAD_UNCHANGED)
+(wH, wW) = overlayImg.shape[:2]
+(B, G, R, A) = cv2.split(overlayImg)
+B = cv2.bitwise_and(B, B, mask=A)
+G = cv2.bitwise_and(G, G, mask=A)
+R = cv2.bitwise_and(R, R, mask=A)
+overlayImg = cv2.merge([B, G, R, A])
+
+# Setup camera
+stream = PiVideoStream(hf=True).start()
+normalDisplay = True
+sleep(2)
+
+# Setup window and mouseCallback event
+cv2.namedWindow(ver)
+cv2.setMouseCallback(ver, control)
+
+# Create a track bar for HoughCircles parameters
+cv2.createTrackbar("dp", ver, 9, 50, placeholder)
+cv2.createTrackbar("param2", ver, 43, 750, placeholder)
+cv2.createTrackbar("minRadius", ver, 20, 200, placeholder)
+cv2.createTrackbar("maxRadius", ver, 70, 250, placeholder)
+
+# Infinite loop
+while True:
+    
+    # Get image from stream
+    frame = stream.read()
+    
+    # Add a 4th dimension (Alpha) to the captured frame
+    (h, w) = frame.shape[:2]
+    frame = numpy.dstack([frame, numpy.ones((h, w), dtype="uint8") * 255])
+
+    # Create an overlay layer
+    overlay = numpy.zeros((h,w,4), "uint8")
+    
+    # Convert into grayscale because HoughCircle only accepts grayscale images
+    bgr2gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Get trackbar position and reflect it in HoughCircles parameters input
+    dp = cv2.getTrackbarPos("dp", ver)
+    param2 = cv2.getTrackbarPos("param2", ver)
+    minRadius = cv2.getTrackbarPos("minRadius", ver)
+    maxRadius = cv2.getTrackbarPos("maxRadius", ver)
+    
+    # Reduce Noise
+    blur = cv2.medianBlur(bgr2gray,5)
+    gaussBlur = cv2.GaussianBlur(blur,(5,5),0)
+
+    # Adaptive threshold allows us to detect sharp edges in images
+    threshold = cv2.adaptiveThreshold(gaussBlur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                      cv2.THRESH_BINARY,11,3.5)
+
+    # Scan for circles
+    circles = cv2.HoughCircles(threshold, cv2.HOUGH_GRADIENT, dp, 396,
+                               191, param2, minRadius, maxRadius)
+    '''
+    Experimental values:            Original Values:
+    dp = 9                          dp = 12
+    minDist = 396                   minDist = 396
+    param1 = 191                    param1 = 191
+    param2 = 43                     param2 = 199
+    minRadius = 20                  minRadius = 5
+    maxRadius = 70                  maxRadius = 70
+    '''
+
+    # If circles are found draw them
+    if circles is not None:
+        circles = numpy.round(circles[0,:]).astype("int")
+        for (x, y, r) in circles:
+            # Resize watermark image
+            resized = cv2.resize(overlayImg, (2*r, 2*r),
+                                 interpolation = cv2.INTER_AREA)
+
+            # Retrieve overlay location
+            y1 = y-r
+            y2 = y+r
+            x1 = x-r
+            x2 = x+r
+
+            # Check whether overlay location is within window resolution
+            if x1>0 and x1<w and x2>0 and x2<w and y1>0 and y1<h and y2>0 and y2<h:
+                # Place overlay image inside circle
+                overlay[y1:y2,x1:x2]=resized
+
+                # Join overlay with live feed and apply specified transparency level
+                output = cv2.addWeighted(overlay, args["alpha"], frame, 1.0, 0)
+
+                # If debug flag is invoked
+                if args["debug"] == 1:
+                    # Draw circle
+                    cv2.circle(output, (x,y),r,(0,255,0),4)
+                
+            # If not within window resolution keep looking
+            else:
+                output = frame
+    
+    # Live feed display toggle
+    if normalDisplay:
+            cv2.imshow(ver, output)
+            key = cv2.waitKey(1) & 0xFF
+    elif not(normalDisplay):
+        cv2.imshow(ver, threshold)
+        key = cv2.waitKey(1) & 0xFF
