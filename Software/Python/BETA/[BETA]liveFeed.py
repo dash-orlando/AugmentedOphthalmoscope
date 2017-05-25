c7b1bc618a8cb7d21701e270d0a5a5a6705955bf
moe93 <m.odeh93@gmail.com>
2017-05-25 13:37:48 -0400

    Incorporated ToF sensor

--- Software/Python/BETA/[BETA]liveFeed.py
+++ Software/Python/BETA/[BETA]liveFeed.py
@@ -5,18 +5,22 @@
 *   -a/--alpha: Specify transperancy level (0.0 - 1.0)
 *   -d/--debug: toggle to enable debugging mode (DEVELOPER ONLY!!!)
 *
-* VERSION: 0.9.5.1
+* VERSION: 0.9.5.2
 *   - Added threads to distribute workload. Program now uses ~70% of
 *     CPU power instead of ~30%
 *   - Increased FPS from 7.5 to 35, a whooping 467% improvement!!!
 *   - Added trackbars to change thresholding method and parameters mid-session
+*   - No overlay is triggered unless at a preset distance
 *
 * KNOWN ISSUES:
-*   - Overlay is lagging the output image (i.e circle is moved to
-*     the left, overlay moves to the left half a second later)
+*   - False circles are detected at times
+*   - When overlay is triggered, frame glitches for a second then resumes
+*     working normaly.
+*   - Threads do NOT safely exit at program shutdown
 *
-* AUTHOR: Mohammad Odeh
-* UPDATED: May 18th, 2017
+* AUTHOR:   Mohammad Odeh
+* WRITTEN:  Aug  1st, 2016
+* UPDATED:  May 25th, 2017
 * ----------------------------------------------------------
 * ----------------------------------------------------------
 *
@@ -24,18 +28,19 @@
 * LEFT CLICK: Toggle view.
 '''
 
-ver = "Live Feed Ver0.9.5.1"
+ver = "Live Feed Ver0.9.5.2"
 print __doc__
 
 # Import necessary modules
 import  numpy, cv2, argparse                                # Various Stuff
 import  RPi.GPIO                    as      GPIO            # GPIO pins for peripherals (i.e LED)
 from    imutils.video.pivideostream import  PiVideoStream   # Import threaded PiCam module
+from    imutils.video               import  FPS             # Benchmark FPS
 from    time                        import  sleep           # Sleep for stability
 from    threading                   import  Thread          # Used to thread processes
 from    Queue                       import  Queue           # Used to queue input/output
 from    timeStamp                   import  fullStamp       # Show date/time on console output
-from    imutils.video               import  FPS             # Benchmark FPS
+from    usbProtocol                 import  createUSBPort   # Create USB Port
 
 # ************************************************************************
 # =====================> CONSTRUCT ARGUMENT PARSER <=====================
@@ -51,7 +56,7 @@ ap.add_argument("-d", "--debug", action='store_true',
 
 args = vars( ap.parse_args() )
 
-args["debug"] = True
+#args["debug"] = True
 # ************************************************************************
 # =====================> DEFINE NECESSARY FUNCTIONS <=====================
 # ************************************************************************
@@ -70,11 +75,12 @@ def control( event, x, y, flags, param ):
             print( fullStamp() + " [INFO] Elapsed time: {:.2f}".format(fps.elapsed()) )
             print( fullStamp() + " [INFO] Approx. FPS : {:.2f}".format(fps.fps()) )
 
-        # Turn off LED
-        GPIO.output( LED, GPIO.LOW )
-        stream.stop()
-        cv2.destroyAllWindows()
-        quit()
+        # Do some shutdown clean up
+        ToF.close()                     # Close port
+        GPIO.output( LED, GPIO.LOW )    # Turn OFF LED
+        stream.stop()                   # Stop capturing frames from stream
+        cv2.destroyAllWindows()         # Close any open windows
+        quit()                          # Shutdown python interpreter
         
     # Left button toggles display
     elif event == cv2.EVENT_LBUTTONDOWN:
@@ -97,26 +103,24 @@ def procFrame(bgr2gray, Q_procFrame):
     # Get trackbar position and reflect it threshold type and values
     threshType = cv2.getTrackbarPos( "Type:\n0.Binary\n1.BinaryInv\n2.Trunc\n3.2_0\n4.2_0Inv",
                     "AI_View")
-    threshold = cv2.getTrackbarPos( "threshold", "AI_View")
+    thresholdVal = cv2.getTrackbarPos( "thresholdVal", "AI_View")
     maxValue = cv2.getTrackbarPos( "maxValue", "AI_View")
 
     # Dissolve noise while maintaining edge sharpness 
-    #bgr2gray = cv2.bilateralFilter( bgr2gray, 11, 17, 17 )
-    bgr2gray = cv2.bilateralFilter( bgr2gray, 5, 17, 17 )
+    bgr2gray = cv2.bilateralFilter( bgr2gray, 5, 17, 17 ) #( bgr2gray, 11, 17, 17 )
     bgr2gray = cv2.GaussianBlur(bgr2gray,(5,5),1)
 
+    # Threshold any color that is not black to white
     if threshType == 0:
-        # Threshold any color that is not black to white
-        #retval, thresholded = cv2.threshold( bgr2gray, 60, 158, cv2.THRESH_BINARY )
-        retval, thresholded = cv2.threshold( bgr2gray, threshold, maxValue, cv2.THRESH_BINARY )
+        retval, thresholded = cv2.threshold( bgr2gray, thresholdVal, maxValue, cv2.THRESH_BINARY )
     elif threshType == 1:
-        retval, thresholded = cv2.threshold( bgr2gray, threshold, maxValue, cv2.THRESH_BINARY_INV )
+        retval, thresholded = cv2.threshold( bgr2gray, thresholdVal, maxValue, cv2.THRESH_BINARY_INV )
     elif threshType == 2:
-        retval, thresholded = cv2.threshold( bgr2gray, threshold, maxValue, cv2.THRESH_TRUNC )
+        retval, thresholded = cv2.threshold( bgr2gray, thresholdVal, maxValue, cv2.THRESH_TRUNC )
     elif threshType == 3:
-        retval, thresholded = cv2.threshold( bgr2gray, threshold, maxValue, cv2.THRESH_TOZERO )
+        retval, thresholded = cv2.threshold( bgr2gray, thresholdVal, maxValue, cv2.THRESH_TOZERO )
     elif threshType == 4:
-        retval, thresholded = cv2.threshold( bgr2gray, threshold, maxValue, cv2.THRESH_TOZERO_INV )
+        retval, thresholded = cv2.threshold( bgr2gray, thresholdVal, maxValue, cv2.THRESH_TOZERO_INV )
 
     kernel = cv2.getStructuringElement( cv2.MORPH_RECT, ( 10, 10 ) )
     bgr2gray = cv2.erode( cv2.dilate( thresholded, kernel, iterations=1 ), kernel, iterations=1 )
@@ -126,9 +130,29 @@ def procFrame(bgr2gray, Q_procFrame):
 
 
 # ******************************************************
+# Define a function to get distance from ToF sensor
+# ******************************************************
+def getDist():
+
+    # No need to reevaluate in the main function at every iteration
+    global ToF_Dist
+
+    # Infinte loop; always listening to serial port
+    while True:
+        if ToF.is_open == False:
+            ToF.open()
+
+        # Do the reading iff there is something available at serial port
+        if ToF.in_waiting > 0:
+            ToF_Dist = int( (ToF.read(size=1).strip('\0')).strip('\n') )
+        else:
+            pass
+
+
+# ******************************************************
 # Define a function to scan for circles from camera feed
 # ******************************************************
-def scan4circles( bgr2gray, overlay, overlayImg, frame, Q ):
+def scan4circles( bgr2gray, overlay, overlayImg, frame, Q_scan4circles ):
 
     # Error handling in case a non-allowable integer is chosen (1)
     try:
@@ -180,13 +204,13 @@ def scan4circles( bgr2gray, overlay, overlayImg, frame, Q ):
 
                 # Place output in queue for retrieval by main thread
                 if Q_scan4circles.full() is False:
-                    Q_scan4circles.put( [output, (x,y,r)] )
+                    Q_scan4circles.put( output )
 
     # Error handling in case a non-allowable integer is chosen (2)
     except Exception as instance:
         print( fullStamp() + " Exception or Error Caught" )
-        print( fullStamp() + " Error Type" + str(type(instance)) + "\n")
-        #print( fullStamp() + " Error Arguments " + str( instance.arg ) + "\n" )
+        print( fullStamp() + " Error Type %s" %str(type(instance)) )
+        #print( fullStamp() + " Error Arguments %s" %str( instance.arg ) )
         print( fullStamp() + " Resetting ALL trackbars..." )
 
         # Reset trackbars
@@ -197,10 +221,10 @@ def scan4circles( bgr2gray, overlay, overlayImg, frame, Q ):
         cv2.createTrackbar( "minRadius", ver, 1, 200, placeholder )
         cv2.createTrackbar( "maxRadius", ver, 14, 250, placeholder )
 
-        print( fullStamp() + " Success\n" )
+        print( fullStamp() + " Success" )
 
         # Exit function and re-loop
-        #return()
+        return()
 
 # ************************************************************************
 # ===========================> SETUP PROGRAM <===========================
@@ -212,6 +236,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(LED,GPIO.OUT)
 GPIO.output(LED,GPIO.HIGH)
+#GPIO.output(LED,GPIO.LOW)
 
 # Check whether an overlay is specified
 if args["overlay"] is not None:
@@ -244,17 +269,30 @@ cv2.createTrackbar( "param2", ver, 291, 750, placeholder ) #143
 cv2.createTrackbar( "minRadius", ver, 1, 200, placeholder )
 cv2.createTrackbar( "maxRadius", ver, 14, 250, placeholder ) #16
 
-# AI view
+# Setup window and trackbars for AI view
 cv2.namedWindow( "AI_View" )
 
 cv2.createTrackbar( "Type:\n0.Binary\n1.BinaryInv\n2.Trunc\n3.2_0\n4.2_0Inv",
                     "AI_View", 3, 4, placeholder )
-cv2.createTrackbar( "threshold", "AI_View", 55, 254, placeholder ) #65
+cv2.createTrackbar( "thresholdVal", "AI_View", 45, 254, placeholder ) #65
 cv2.createTrackbar( "maxValue", "AI_View", 255, 255, placeholder )
 
+# Initialize ToF sensor
+deviceName, port, baudRate = "VL6180", 0, 115200
+ToF = createUSBPort( deviceName, port, baudRate, 3 )
+if ToF.is_open == False:
+    ToF.open()
+ToF.close()
+ToF_Dist = 0    # Initialize to OFF
+
 # Create a queue for retrieving data from thread
-Q_procFrame = Queue( maxsize=0 )
-Q_scan4circles = Queue( maxsize=0 )
+Q_procFrame     = Queue( maxsize=0 )
+Q_scan4circles  = Queue( maxsize=0 )
+
+# Start listening to serial port
+t_getDist = Thread( target=getDist, args=() )
+t_getDist.daemon = True
+t_getDist.start()
 
 # If debug flag is invoked
 if args["debug"]:
@@ -266,8 +304,6 @@ if args["debug"]:
 # ************************************************************************
 # =========================> MAKE IT ALL HAPPEN <=========================
 # ************************************************************************
-
-##initRun = True
 
 # Infinite loop
 while True:
@@ -282,7 +318,7 @@ while True:
 
     # Create an overlay layer
     overlay = numpy.zeros( ( h, w, 4 ), "uint8" )
-    
+
     # Convert into grayscale because HoughCircle only accepts grayscale images
     bgr2gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
 
@@ -294,26 +330,6 @@ while True:
     # Check if queue has something available for retrieval
     if Q_procFrame.qsize() > 0:
         bgr2gray = Q_procFrame.get()
-##        bgr2grayBAK = bgr2gray
-##        if initRun==False:
-##            if abs(oldx - x_ROI) > 10 or abs(oldy - y_ROI) > 10:
-##                # Calculate Region of interest constraints
-##                x1_ROI = x_ROI-r_ROI-10
-##                x2_ROI = x_ROI+r_ROI+10
-##                y1_ROI = y_ROI-r_ROI-10
-##                y2_ROI = y_ROI+r_ROI+10
-##
-##                oldx = x_ROI
-##                oldy = y_ROI
-##                print"updated xyROI"
-##
-##            if x1_ROI>0 and x1_ROI<w and x2_ROI>0 and x2_ROI<w and y1_ROI>0 and y1_ROI<h and y2_ROI>0 and y2_ROI<h:
-##                print("xyr_ROI: " , (x_ROI, y_ROI, r_ROI))
-##                print("ROI Location: ", (y_ROI-r_ROI), (y_ROI+r_ROI), (x_ROI-r_ROI), (x_ROI+r_ROI))
-##                print("BGR shape: ", bgr2gray.shape)
-##                bgr2gray = bgr2gray[ y1_ROI:y2_ROI, x1_ROI:x2_ROI]
-##            else:
-##                bgr2gray = bgr2grayBAK
 
     # Get trackbar position and reflect it in HoughCircles parameters input
     dp = cv2.getTrackbarPos( "dp", ver )
@@ -322,25 +338,19 @@ while True:
     param2 = cv2.getTrackbarPos( "param2", ver )
     minRadius = cv2.getTrackbarPos( "minRadius", ver )
     maxRadius = cv2.getTrackbarPos( "maxRadius", ver )
-    
-    # Start thread to scan for circles
-    t_scan4circles = Thread( target=scan4circles, args=( bgr2gray, overlay, overlayImg, frame, Q_scan4circles ) )
-    t_scan4circles.daemon = True
-    t_scan4circles.start()
 
-    # Check if queue has something available for retrieval
-    if Q_scan4circles.qsize() > 0:
-        output, (x_ROI, y_ROI, r_ROI) = Q_scan4circles.get()
-##        if initRun==True:
-##            initRun=False
-##
-##            x1_ROI = x_ROI-r_ROI-10
-##            x2_ROI = x_ROI+r_ROI+10
-##            y1_ROI = y_ROI-r_ROI-10
-##            y2_ROI = y_ROI+r_ROI+10
-##
-##            oldx = x_ROI
-##            oldy = y_ROI
+    # If within scan distance call thread to scan for circles
+    if ToF_Dist == 1:
+        # Start thread to scan for circles
+        t_scan4circles = Thread( target=scan4circles, args=( bgr2gray, overlay, overlayImg, frame, Q_scan4circles ) )
+        t_scan4circles.daemon = True
+        t_scan4circles.start()
+
+        # Check if queue has something available for retrieval
+        if Q_scan4circles.qsize() > 0:
+            output = Q_scan4circles.get()
+    else:
+        output = frame
 
     # If debug flag is invoked
     if args["debug"]:
@@ -468,5 +478,59 @@ for ( x, y, r ) in circles:
             # Resize watermark image
             resized = cv2.resize( overlayImg, ( 2*r, 2*r ),
                                   interpolation = cv2.INTER_AREA )
+
+------------------------------------------------------------------------------------------------
+
+    # Check if queue has something available for retrieval
+    if Q_procFrame.qsize() > 0:
+        bgr2gray = Q_procFrame.get()
+##        bgr2grayBAK = bgr2gray
+##        if initRun==False:
+##            if abs(oldx - x_ROI) > 10 or abs(oldy - y_ROI) > 10:
+##                # Calculate Region of interest constraints
+##                x1_ROI = x_ROI-r_ROI-10
+##                x2_ROI = x_ROI+r_ROI+10
+##                y1_ROI = y_ROI-r_ROI-10
+##                y2_ROI = y_ROI+r_ROI+10
+##
+##                oldx = x_ROI
+##                oldy = y_ROI
+##                print"updated xyROI"
+##
+##            if x1_ROI>0 and x1_ROI<w and x2_ROI>0 and x2_ROI<w and y1_ROI>0 and y1_ROI<h and y2_ROI>0 and y2_ROI<h:
+##                print("xyr_ROI: " , (x_ROI, y_ROI, r_ROI))
+##                print("ROI Location: ", (y_ROI-r_ROI), (y_ROI+r_ROI), (x_ROI-r_ROI), (x_ROI+r_ROI))
+##                print("BGR shape: ", bgr2gray.shape)
+##                bgr2gray = bgr2gray[ y1_ROI:y2_ROI, x1_ROI:x2_ROI]
+##            else:
+##                bgr2gray = bgr2grayBAK
+
+    # Get trackbar position and reflect it in HoughCircles parameters input
+    dp = cv2.getTrackbarPos( "dp", ver )
+    minDist = cv2.getTrackbarPos( "minDist", ver )
+    param1 = cv2.getTrackbarPos( "param1", ver )
+    param2 = cv2.getTrackbarPos( "param2", ver )
+    minRadius = cv2.getTrackbarPos( "minRadius", ver )
+    maxRadius = cv2.getTrackbarPos( "maxRadius", ver )
+
+-----------------------------------------------------------------------------------------------------
+
+    # Check if queue has something available for retrieval
+    if Q_scan4circles.qsize() > 0:
+        output, (x_ROI, y_ROI, r_ROI) = Q_scan4circles.get()
+##        if initRun==True:
+##            initRun=False
+##
+##            x1_ROI = x_ROI-r_ROI-10
+##            x2_ROI = x_ROI+r_ROI+10
+##            y1_ROI = y_ROI-r_ROI-10
+##            y2_ROI = y_ROI+r_ROI+10
+##
+##            oldx = x_ROI
+##            oldy = y_ROI
+
+    # If debug flag is invoked
+    if args["debug"]:
+       fps.update()
 
 '''
---
+++ Software/Python/usbProtocol.py
@@ -0,0 +1,148 @@
+"""
+usbProtocol.py
+
+The following module has been created to manage the bluetooth interface between the control system and the connected devices
+
+Michael Xynidis
+Fluvio L Lobo Fenoglietto
+09/01/2016
+
+
+"""
+
+# Import Libraries and/or Modules
+import bluetooth
+"""
+        Implementation of the "bluetooth" module may require the installation of the python-bluez package
+        >> sudp apt-get install python-bluez
+"""
+import os
+import serial
+import time
+from timeStamp import *
+
+# Find RF Device
+#   This function uses the hardware of the peripheral device or control system to scan/find bluetooth enabled devices
+#   This function does not differenciate among found devices
+#   Input   ::  None
+#   Output  ::  {array, list} "availableDeviceNames", "availableDeviceBTAddresses"
+def findDevices():
+    print fullStamp() + " findDevices()"
+    devices = bluetooth.discover_devices(
+        duration=20,                                                                        # Search timeout
+        lookup_names=True)                                                                  # Search and acquire names of antennas
+    Ndevices = len(devices)                                                                 # Number of detected devices
+    availableDeviceNames = []                                                               # Initialized arrays/lists for device names...
+    availableDeviceBTAddresses = []                                                         # ...and their bluetooth addresses
+    for i in range(0,Ndevices):                                                             # Populate device name and bluetooth address arrays/lists with a for-loop
+        availableDeviceNames.append(devices[i][1])
+        availableDeviceBTAddresses.append(devices[i][0])
+    print fullStamp() + " Devices found (names): " + str(availableDeviceNames)              # Print the list of devices found
+    print fullStamp() + " Devices found (addresses): " + str(availableDeviceBTAddresses)    # Print the list of addresses for the devices found
+    return availableDeviceNames, availableDeviceBTAddresses                                 # Return arrays/lists of devices and bluetooth addresses
+
+# Identify Smart Devices - General
+#   This function searches through the list of detected devices and finds the smart devices corresponding to the input identifier
+#   Input   ::  {string}     "smartDeviceIdentifier"
+#           ::  {array/list} "availableDeviceNames", "availableDeviceBTAddresses"
+#   Output  ::  {array/list} "smartDeviceNames", "smartDeviceBTAddresses"
+def findSmartDevices(smartDeviceIdentifier, availableDeviceNames, availableDeviceBTAddresses):
+    print fullStamp() + " findSmartDevices()"
+    Ndevices = len(availableDeviceNames)
+    smartDeviceNames = []
+    smartDeviceBTAddresses = []
+    for i in range(0,Ndevices):
+        deviceIdentifier = availableDeviceNames[i][0:4]
+        if deviceIdentifier == smartDeviceIdentifier:
+            smartDeviceNames.append(availableDeviceNames[i])
+            smartDeviceBTAddresses.append(availableDeviceBTAddresses[i])
+    print fullStamp() + " Smart Devices found (names): " + str(smartDeviceNames)
+    print fullStamp() + " Smart Devices found (addresses): " + str(smartDeviceBTAddresses)
+    return smartDeviceNames, smartDeviceBTAddresses
+
+# Identify Smart Device - Specific
+#   This function searches through the list of detected devices and finds the specific smart device corresponding to the input name
+#   Input   ::  {string}     "smartDeviceName"
+#           ::  {array/list} "availableDeviceNames", "availableDeviceBTAddresses"
+#   Output  ::  {array/list} "smartDeviceNames", "smartDeviceBTAddresses"
+def findSmartDevice(smartDeviceName, availableDeviceNames, availableDeviceBTAddresses):
+    print fullStamp() + " findSmartDevices()"
+    Ndevices = len(availableDeviceNames)
+    smartDeviceNames = []
+    smartDeviceBTAddresses = []
+    for i in range(0,Ndevices):
+        deviceName = availableDeviceNames[i]
+        if deviceName == smartDeviceName:
+            smartDeviceNames.append(availableDeviceNames[i])
+            smartDeviceBTAddresses.append(availableDeviceBTAddresses[i])
+    print fullStamp() + " Smart Devices found (names): " + str(smartDeviceNames)
+    print fullStamp() + " Smart Devices found (addresses): " + str(smartDeviceBTAddresses)
+    return smartDeviceNames, smartDeviceBTAddresses                                                                      # Return RFObject or list of objects
+
+# Create USB Port
+def createPort(portNumber,baudrate,timeout):
+    rfObject = serial.Serial(
+        port = "/dev/ttyUSB" + str(portNumber),
+        baudrate = baudrate,
+        timeout = timeout)
+    return rfObject
+
+# Create USB Port
+def createUSBPort(deviceName,portNumber,baudrate,attempts):
+    print fullStamp() + " createUSBPort()"
+    usbObject = serial.Serial(
+        port = "/dev/ttyUSB" + str(portNumber),
+        baudrate = baudrate)
+    time.sleep(1)
+    #usbConnectionCheck(usbObject,deviceName,portNumber,baudrate,attempts)
+    usbObject.close()
+    return usbObject
+
+# Connection Check -Simple
+#   Simplest variant of the connection check functions
+def usbConnectionCheck(usbObject,deviceName,portNumber,baudrate,attempts):
+    print fullStamp() + " usbConnectionCheck()"
+    time.sleep(1)
+    if usbObject.is_open == False:
+        usbObject.open()
+    print fullStamp() + " Requesting Device Name"
+    usbObject.write('n')
+    time.sleep(1)
+    inString = usbObject.readline()[:-1]
+    print inString
+    if inString == deviceName:
+        print fullStamp() + " Connection successfully established with " + deviceName
+    else:
+        usbObject.close()
+        if attempts is not 0:
+            return createUSBPort(deviceName,portNumber,baudrate,attempts-1)
+        elif attempts is 0:
+            print fullStamp() + " Connection Attempts Limit Reached"
+            print fullStamp() + " Please troubleshoot " + deviceName
+
+# Send Until ReaD
+#       This function sends an input command through the rfcomm port to the remote device
+#       The function sends such command persistently until a timeout or iteration check are met
+#       Input   ::      rfObject                {object}        serial object
+#                       outByte                 {chr}           command in characters/bytes
+#                       timeout                 {int}           maximum wait time for serial communication
+#                       iterCheck               {int}           maximum number of iterations for serial communication
+#       Output  ::      inByte                  {chr}           response from remote device in characters/bytes
+#                       terminal messages       {string}        terminal messages for logging      
+def sendUntilRead(rfObject, outByte, timeout, iterCheck):
+    print fullStamp() + " sendUntilRead()"                                                                  # Printing program name
+    iterCount = 0
+    startTime = time.time()                                                                                 # Initial time, instance before entering "while loop"
+    while (time.time() - startTime) < timeout and iterCount <= iterCheck:                                   # While loop - will continue until either timeout or iteration check is reached
+        print fullStamp() + " Communication attempt " + str(iterCount) + "/" + str(iterCheck)
+        print fullStamp() + " Time = " + str(time.time()-startTime)
+        rfObject.write(outByte)                                                                             # Send CHK / System Check request
+        inByte = rfObject.read()                                                                            # Read response from remote device
+        if inByte == definitions.ACK:                                                                       # If response equals ACK / Positive Acknowledgement
+            # print fullStamp() + " ACK"                                                                    # Print terminal message, device READY / System Check Successful                                                                             
+            return inByte                                                                                   # Return the byte read from the port
+            break                                                                                           # Break out of the "while loop"
+        elif inByte == definitions.NAK:                                                                     # If response equals NAK / Negative Acknowledgement
+            # print fullStamp() + " NAK"                                                                    # Print terminal message, device NOT READY / System Check Failed
+            return inByte                                                                                   # Return the byte read from the port
+            break                                                                                           # Break out of the "while loop"
