70628df19927d5875bfebbffb666e5586835be2a
moe93 <m.odeh93@gmail.com>
2018-05-22 16:05:11 -0400

    Modify thresholding values

--- Software/Python/BETA/[BETA]liveFeed.py
+++ Software/Python/BETA/[BETA]liveFeed.py
@@ -10,12 +10,13 @@
 *   -a/--alpha  : Specify transperancy level (0.0 - 1.0)
 *   -d/--debug  : Enable debugging
 *
-* VERSION: 1.0b
-*   - ADDED   : Initial release 
+* VERSION: 1.1.1b
+*   - ADDED   : Overlay an image/pathology
+*   - MODIFIED: Significantly reduced false-positives
 *
 * KNOWN ISSUES:
 *   - Code is VERY buggy and lacks many of the previous
-*     functionalities (i.e overlays, threading, etc...)
+*     functionalities (i.e threading, etc...)
 *
 * AUTHOR                    :   Mohammad Odeh
 * WRITTEN                   :   Aug   1st, 2016 Year of Our Lord
@@ -74,9 +75,9 @@ def control( event, x, y, flags, param ):
             cv2.destroyAllWindows()                                         # Close any open windows
             
         except Exception as error:
-            print( "{} Error caught in control()".format(FS()) )            # Specify error type
-            print( "{0} {1}".format(FS(), type(error)) )                    # ...
-            pass
+            if( ["debug"] ):
+                print( "{} Error caught in control()".format(FS()) )        # Specify error type
+                print( "{0} {1}".format(FS(), type(error)) )                # ...
 
         finally: 
             quit()                                                          # Shutdown python interpreter
@@ -163,18 +164,18 @@ def procFrame( image ):
         
 # ------------------------------------------------------------------------
 
-def scan4circles( processed, overlay, overlayImg, frame ):
+def scan4circles( processed, overlay_frame, overlay_img, frame ):
     '''
     Scan for circles within an image
 
-    INPUTS:
-        - processed : Processed image
-        - overlay   : The overlay empty frame
-        - overlayImg: The overlay image
-        - frame     : Frame to which we should attach overlay
+    INPUTS:-
+        - processed     : Processed image
+        - overlay_frame : The overlay empty frame
+        - overlay_img   : The overlay image
+        - frame         : Frame to which we should attach overlay
 
-    OUTPUT:
-        - output    : Image with/without overlay
+    OUTPUT:-
+        - frame         : Image with/without overlay
                 (depends whether circles were found or not)
     '''
     
@@ -186,40 +187,79 @@ def scan4circles( processed, overlay, overlayImg, frame ):
         _, contours, _ = cv2.findContours( processed,                       # Find contours based on their...
                                            cv2.RETR_EXTERNAL,               # external edges and keep only...
                                            cv2.CHAIN_APPROX_SIMPLE )        # intermediate points (SIMPLE)
-
+        
         # If circles are found draw them
-        if( contours is not None ):                                         # Check if we detected anything
+        if( len(contours) > 0 ):                                            # Check if we detected anything
             for c in contours:                                              # Iterate over all contours
                 (x,y),r = cv2.minEnclosingCircle(c)                         # 
                 center = (int(x),int(y))                                    # Get circle's center...
                 r = int(r)                                                  # and radius
                 
                 if( r_min <= r and r <= r_max ):                            # Check if within desired limit
-                    output = cv2.addWeighted( overlay,                      # Join overlay with the...
-                                              args["alpha"],                # livefeed frame and apply...
-                                              frame, 1.0, 0 )               # specified alpha levels.
-                    cv2.circle( frame, center, r, (0, 255, 0), 2 )          # draw a circle
-
-                else:                                                       # If no contour is within the...
-                    output = frame                                          # limits, return original frame
+                    pos = ( center, r )                                     # Pack co-ordinates
+                    hull = cv2.convexHull(c)
+##                    print( cv2.isContourConvex(c) )
+##                    cv2.polylines( frame, hull, True, (0,255,0), 3)
+                    frame = add_overlay( overlay_frame,                     # Add overlay
+                                         overlay_img,                       # ...
+                                         frame, pos, )                      # ...
                     
-        else:                                                               # If no contours were found...
-            output = frame                                                  # return original frame
+                else: pass                                                  # Nothing is found
+        else: pass                                                          # ...
 
-        return( output )                                                    # Return
+        return( frame )
 
     # Error handling in case a non-allowable integer is chosen (2)
     except Exception as error:
-        print( "{} Error caught in scan4circles()".format(FS()) )           # Specify error type
-        print( "{0} {1}".format(FS(), error) )                              # ...
+        if( ["debug"] ):
+            print( "{} Error caught in scan4circles()".format(FS()) )       # Specify error type
+            print( "{0} {1}".format(FS(), error) )                          # ...
 
-        # Reset trackbars
         cv2.createTrackbar( "minRadius" , ver, 15   , 150, placeholder )    # Reset trackbars
-        cv2.createTrackbar( "maxRadius" , ver, 25   , 150, placeholder )    # ...
+        cv2.createTrackbar( "maxRadius" , ver, 35   , 150, placeholder )    # ...
 
-        # Exit function and re-loop
         return( None )                                                      # Return NONE
     
+# ------------------------------------------------------------------------
+
+def add_overlay( overlay_frame, overlay_img, frame, pos ):
+    '''
+    Resize and add overlay image into detected pupil location 
+
+    INPUTS:
+        - overlay_frame : The overlay empty frame
+        - overlay_img   : The overlay image
+        - frame         : Frame to which we should attach overlay
+        - pos           : Co-ordinates where pupil is
+
+    OUTPUT:
+        - frame         : Image with overlay
+    '''
+    
+    (x, y), r = pos                                                         # Unpack co-ordinates
+    
+    x_min, x_max = x-r, x+r                                                 # Find min/max x-range
+    y_min, y_max = y-r, y+r                                                 # Find min/max y-range
+
+    if( x_min > 0 and y_min > 0 ):
+        if( x_max < w and y_max < h ):
+##            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+##            keypoints = detector.detect(gray)
+##            for k in keypoints:
+##            cv2.circle(frame, (int(k.pt[0]), int(k.pt[1])), int(k.size/2), (0, 255, 0), 2)
+            overlay_img = cv2.resize( overlay_img, ( 2*r, 2*r ),            # Resize overlay image to fit
+                                      interpolation = cv2.INTER_AREA )      # ...
+            
+            overlay_frame[ y_min:y_max, x_min:x_max] = overlay_img          # Place overlay image into overlay frame
+
+            frame = cv2.addWeighted( overlay_frame,                         # Join overlay frame (alpha)
+                                     args["alpha"],                         # with actual frame (RGB)
+                                     frame, 1.0, 0 )                        # ...
+
+            if( ["debug"] ):
+                cv2.circle( frame, (x, y), r, (0, 255, 0), 2 )              # Draw a circle
+            
+    return( frame )
 
 # ************************************************************************
 # ===========================> SETUP PROGRAM <===========================
@@ -250,25 +290,56 @@ colorWipe(strip, Color(255, 255, 255, 255), 0)                              # Tu
 cv2.namedWindow( ver )                                                      # Start a named window for output
 cv2.setMouseCallback( ver, control )                                        # Connect mouse events to actions
 cv2.createTrackbar( "minRadius" , ver, 15   , 150, placeholder )            # Trackbars for min and max radii
-cv2.createTrackbar( "maxRadius" , ver, 25   , 150, placeholder )            #     (used in scan4circles)
+cv2.createTrackbar( "maxRadius" , ver, 35   , 150, placeholder )            #     (used in scan4circles)
 
 # Setup window and trackbars for CV window
 cv2.namedWindow( "CV" )                                                     # Start a named window for CV
 
 cv2.createTrackbar( "Type:\n0.MEAN_Binary\n1.GAUSSIAN_Binary\n2.MEAN_BinaryInv\n3.GAUSSIAN_BinaryInv\n4.OTSU",
-                    "CV", 3, 3, placeholder )                               # ...
-cv2.createTrackbar( "maxValue"      , "CV", 245, 255, placeholder )         # Trackbars to modify adaptive
-cv2.createTrackbar( "blockSize"     , "CV", 230, 254, placeholder )         # thresholding parameters
-cv2.createTrackbar( "cte"           , "CV", 8  , 100, placeholder )         #   (used in procFrame)
-cv2.createTrackbar( "GaussianBlur"  , "CV", 26 , 50 , placeholder )         # ...
+                    "CV", 0, 3, placeholder )#3                               # ...
+cv2.createTrackbar( "maxValue"      , "CV", 255, 255, placeholder )         # Trackbars to modify adaptive
+cv2.createTrackbar( "blockSize"     , "CV", 245, 254, placeholder )#230         # thresholding parameters
+cv2.createTrackbar( "cte"           , "CV", 2  , 100, placeholder )#8         #   (used in procFrame)
+cv2.createTrackbar( "GaussianBlur"  , "CV", 16 , 50 , placeholder )#26         # ...
+
+### Setup BlobDetector
+##detector = cv2.SimpleBlobDetector_create()
+##params = cv2.SimpleBlobDetector_Params()
+##	 
+### Filter by Area.
+##params.filterByArea = True
+##params.minArea = 3.141 * 15**2#20000
+##params.maxArea = 3.141 * 40**2#40000
+##	 
+### Filter by Circularity
+##params.filterByCircularity = True
+##params.minCircularity = 0.25
+## 
+### Filter by Convexity
+####params.filterByConvexity = True
+####params.minConvexity = 0.87
+##	 
+### Filter by Inertia
+##params.filterByInertia = True
+##params.minInertiaRatio = 0.8
+##
+### Distance Between Blobs
+##params.minDistBetweenBlobs = 1000
+##	 
+### Create a detector with the parameters
+##detector = cv2.SimpleBlobDetector_create(params)
+
     
 # ************************************************************************
 # =========================> MAKE IT ALL HAPPEN <=========================
 # ************************************************************************
 global lower_bound, upper_bound
 
-lower_bound = np.array( [0,0,10] )
-upper_bound = np.array( [255,255,195] )
+##lower_bound = np.array( [0,0,10] )
+##upper_bound = np.array( [255,255,195] )
+
+lower_bound = np.array( [0,0,0] )
+upper_bound = np.array( [10,10,10] )
 
 while( True ):
     # Capture frame
